4ab138997a38f7f613623329b6a11017ea9c1f64
vshubham331998 <vshubham331998@gmail.com>
2023-08-12 14:58:48 +0530

    Made Changes in Submission

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,16 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
+
+    public Boat() {
+
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -30,6 +30,10 @@ public class Vehicle {
         System.out.println("stop method called - The vehicle is stopped");
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
